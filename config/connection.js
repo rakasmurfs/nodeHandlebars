1bd97293f3ce75367cfdf8f339cd0b308a489339
Miguel Wolmers <rakasmurfs@gmail.com>
2019-11-10 14:22:45 -0500

    connection.connect

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,18 @@ if (process.env.JAWSDB_URL){
     console.log("Using environment:", process.env.JAWSDB_URL);
     try{
         connection = mysql.createConnection(process.env.JAWSDB_URL);
+        connection.connect();
     }
     catch(error){
         console.log(error);
     }
 } else {
     connection = mysql.createConnection({
-        host: "lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        port: 3306,
-        user: "r7zlq1pxkngin7j9",
-        password: "w8l3yle0eadtwsn9",
-        database: "hd2lkn1ecyf02j24"
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
     });
 }
 
